dbc4f64feda53920520412be71cd56a6cbc1b3cb
Jaerker <johanjakberger@gmail.com>
2024-03-21 07:48:20 +0100

    small bug fixes

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,6 @@ window.addEventListener('load', async () => {
     //Dessa är här om man inte har någon info i localStorage, så man uppdaterar den korrekt!
     // await api.user.list();
     // await api.product.list();
-    api.user.login('jesper123');
     // api.orderHistory.list();
 
     const currentUser = await api.user.details(api.user.getCurrentUser());
@@ -20,7 +19,7 @@ window.addEventListener('load', async () => {
     const loader = document.querySelector('.loader');
         loader.classList.add('loader-hidden'); // Ta bort punkten från "loader-hidden"
         loader.addEventListener('transitionend', () => { // Ta bort punkten efter "transitionend" och använd ett argument för att hantera händelsen
-            document.body.removeChild(loader); // Ta bort citatet runt 'loader' och använd variabeln direkt
+            loader.remove(); // Ta bort citatet runt 'loader' och använd variabeln direkt
         });
 
 
